91ae759df296ca8f547fbcb95217f18d29cb35a7
Edson Correia <edsoncorreia997@gmail.com>
2022-11-30 00:32:42 -0300

    finished project

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.banner .wrapper', {duration: 1500});
+sr.reveal('.wrapper-projetos', {duration: 1500});
+sr.reveal('.box-pages.-second', {duration: 1500});
+sr.reveal('.box-pages.-first', {duration: 1500});
